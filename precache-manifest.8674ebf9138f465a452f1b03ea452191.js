3fdaeba5a17ba8eeabc5255b7a69698ebf2474e8
AndriyPolukhin <andriy.polukhin@gmail.com>
2019-12-29 21:20:51 +0200

    Updates

---
+++ precache-manifest.8674ebf9138f465a452f1b03ea452191.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3eeee6077108c2d5bf43667b57dfb72e",
+    "url": "/rolodex/index.html"
+  },
+  {
+    "revision": "d283f3bd0cc6286f99d7",
+    "url": "/rolodex/static/css/main.2830b791.chunk.css"
+  },
+  {
+    "revision": "ff0178b2c039e839d26a",
+    "url": "/rolodex/static/js/2.0a2f46cf.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/rolodex/static/js/2.0a2f46cf.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d283f3bd0cc6286f99d7",
+    "url": "/rolodex/static/js/main.01f37433.chunk.js"
+  },
+  {
+    "revision": "f092306fb1ae0ff8129f",
+    "url": "/rolodex/static/js/runtime-main.aea65a69.js"
+  }
+]);
